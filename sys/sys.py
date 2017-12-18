50fd6a7851b193f39cbe353dcfd2ced573c51238
jiaq187 <jiaq187@126.com>
2017-12-18 19:12:04 +0800

    运维常用模块二：os

--- sys/sys.py
+++ sys/sys.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import sys
-print(sys.argv[0])
-print(sys.argv[1])
-print(sys.argv[2])
-print(sys.argv[3])
-print(sys.argv[4])
-print(sys.argv[5])
-print(len(sys.argv))
-sys.getwindowsversion()
+# !/usr/bin/env python
+# coding:UTF-8
+import MySQLdb
+
+try:
+    conn = MySQLdb.connect(host="localhost", user='root', passwd='123', db='test')
+    cursor = conn.cursor()  # 创建一个游标对象
+    sql = "select * from user"
+    cursor.execute(sql)  # 执行一个sql（游标对象操作）
+    for i in cursor.fetchall():  # 列出执行的结果
+        print
+        i
+    conn.commit()  # 事务提交（对数据库修改必须使用这个方法，否则数据不会被真正写入）
+    cursor.close()  # 关闭游标
+    conn.close()  # 关闭数据库连接
+exceptException, e:
+print
+"connection error:" + str(e)
