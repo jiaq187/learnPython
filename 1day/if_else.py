895708821bbc3b8179d5e3bb611cf8ba7f6ae0ef
jiaq187 <jiaq187@126.com>
2018-01-24 15:57:20 +0800

    运维常用模块二：os

---
+++ 1day/if_else.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+sex = input("plese input your gender:")
+if  sex == "girl":
+    print("I would like to have a little")
+    print("the second line")
+elif sex == "man":
+    print("Going to homesexual!...")
+else:
+    print("Perwor!")
+
+#
