7cca2ee29cc5335e19f900cd6846a8272f088c49
jiaq187 <jiaq187@126.com>
2018-01-28 20:19:44 +0800

    1月26日

---
+++ 2day/builtclass.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''page = 10
+all_iterm = 95
+result = all_iterm.__rdivmod__(12)
+print(result)
+
+name = 'eric'
+resut = name.capitalize()
+print(resut)
+'''
+
+name = 'loving you'
+#result = name.center(20,"*")
+#result = name.count('ad',0,30)
+#result = name.encode('gbk')
+result = name.endswith('u',0,9)
+print(result)
